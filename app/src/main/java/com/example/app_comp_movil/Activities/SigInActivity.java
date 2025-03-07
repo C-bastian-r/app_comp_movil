ee23e38209f3e719c302e97b5cd0bc4094853678
cbastian <junac.bastian@gmail.com>
2025-03-07 13:37:21 -0500

    commit 07-03-2025

--- app/src/main/java/com/example/app_comp_movil/Activities/SigInActivity.java
+++ app/src/main/java/com/example/app_comp_movil/Activities/SigInActivity.java
@@ -16,11 +16,15 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.app_comp_movil.Model.DAOs.UserDAO;
 import com.example.app_comp_movil.Model.Entities.UserEntity;
 import com.example.app_comp_movil.R;
+import com.example.app_comp_movil.Utils.Navigation.IntentNavigator;
 import com.example.app_comp_movil.Utils.Security.SecurityModule;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class SigInActivity extends AppCompatActivity {
 
     private SecurityModule securityModule;
+    private IntentNavigator navigator;
+
     private EditText etSiginUserName;
     private EditText etSiginEmail;
     private EditText etSiginPassword;
@@ -28,6 +32,8 @@ public class SigInActivity extends AppCompatActivity {
 
     private void init(){
         securityModule = new SecurityModule();
+        navigator = new IntentNavigator(this);
+
         etSiginUserName = findViewById(R.id.etSiginUserName);
         etSiginEmail = findViewById(R.id.etSiginEmail);
         etSiginPassword = findViewById(R.id.etSiginPassword);
@@ -44,6 +50,15 @@ public class SigInActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        MaterialToolbar toolbar = findViewById(R.id.top_app_bar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_action_back);
+        toolbar.setNavigationOnClickListener(v -> {
+            onBackPressed();
+        });
+
         init();
         btnSiginContinue.setOnClickListener(this::register);
     }
@@ -58,6 +73,7 @@ public class SigInActivity extends AppCompatActivity {
                 UserEntity user = new UserEntity(userName, email, password);
                 userDAO.getInsertUser(user);
                 Toast.makeText(this, "usuario "+userName+" registrado con exito!", Toast.LENGTH_LONG).show();
+                navigator.setIntentNav(LoginActivity.class);
             }else {
                 Toast.makeText(this, "el formato del correo no es valido", Toast.LENGTH_LONG).show();
             }
--- app/src/main/java/com/example/app_comp_movil/Model/Entities/UserEntity.java
+++ app/src/main/java/com/example/app_comp_movil/Model/Entities/UserEntity.java
@@ -1,10 +1,12 @@
 package com.example.app_comp_movil.Model.Entities;
 
 public class UserEntity {
+    private int userId;
     private String userName;
     private String email;
     private String password;
 
+    //region constructores
     public UserEntity() {
     }
 
@@ -14,7 +16,24 @@ public class UserEntity {
         this.password = password;
     }
 
+    public UserEntity(int userId, String userName, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+    //endregion
+
     //region getters y setters
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
     public String getUserName() {
         return userName;
